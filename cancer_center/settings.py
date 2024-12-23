c29c4febf9eded368fec7d30435496d79276f07c
aidikhcc <157950596+aidikhcc@users.noreply.github.com>
2024-12-23 14:25:08 +0300

    Update settings.py

--- cancer_center/settings.py
+++ cancer_center/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-your-secret-key-here'
-
+DEBUG=TRUE
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
